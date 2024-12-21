25e11375c152fbed25595140a6b42065c25a45aa
kyeonkim <kyeonkim5021@gmail.com>
2024-12-21 18:24:13 +0900

    feat: Jwt 토큰 생성 및 테스트

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  generateAccessToken(payload: object): string {
+    const accessTokenSecret = this.configService.get<string>('ACCESS_TOKEN_SECRET');
+    return this.jwtService.sign(payload, {
+      secret: accessTokenSecret,
+      expiresIn: '1m',
+    });
+  }
+
+  generateRefreshToken(payload: object): string {
+    const refreshTokenSecret = this.configService.get<string>('REFRESH_TOKEN_SECRET');
+    return this.jwtService.sign(payload, {
+      secret: refreshTokenSecret,
+      expiresIn: '1h',
+    });
+  }
+}
